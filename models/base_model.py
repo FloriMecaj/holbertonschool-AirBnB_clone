5f9f631823f24fbc510c75a31ca0504620fdad47
FloriMecaj <florimecaj31@gmail.com>
2024-02-26 14:20:10 +0100

    Basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python3
+
 """Class BaseModel that defines all common attributes/methods for other classes"""
 
 
+from uuid import uuid4
+import datetime
+
 class BaseModel:
+    """class Basemodel"""
+
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+        
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
