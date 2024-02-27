626a35cf0f9835286f438d92eb2f9cc3f9262a7a
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-27 11:05:06 +0100

    add files

---
+++ models/engine/file_storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Write a class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances"""
+
+
+class FileStorage:
+    '''file storage class'''
+
