5d9c28559335f6379b6a57291926caecd2e37f53
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-29 13:38:55 +0100

    add console

--- console.py
+++ console.py
@@ -64,6 +64,59 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print(storage.all()[key])
 
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id (save the change into the JSON file).
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                del storage.all()[key]
+                storage.save()
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name.
+        """
+        if arg and arg != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            for key, value in storage.all().items():
+                if not arg or arg == key.split(".")[0]:
+                    print(str(value))
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file).
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key not in storage.all():
+                print("** no instance found **")
+            elif len(args) == 2:
+                print("** attribute name missing **")
+            elif len(args) == 3:
+                print("** value missing **")
+            else:
+                setattr(storage.all()[key], args[2], args[3])
+                storage.save()
+
 if __name__ == '__main__':
     interpreter = HBNBCommand()
     interpreter.cmdloop()
