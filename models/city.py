d0aa4809d17b25c36222f88ae77c3b5cbdbf50ef
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-01 09:52:34 +0100

    fix all pycodestyle checks for ex 1

--- models/city.py
+++ models/city.py
@@ -6,6 +6,7 @@ This script defines a BaseModel class for managing and persisting data.
 
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
     """class city"""
     state_id = ""
--- models/review.py
+++ models/review.py
@@ -6,6 +6,7 @@ This script defines a BaseModel class for managing and persisting data.
 
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
     """class review"""
     place_id = ""
