be2628f29b4968671fa82e43d49616e0915601a9
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-01 09:42:50 +0100

    add more classes ex 9, all inherit from BaseModel

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This script defines a BaseModel class for managing and persisting data.
+"""
+
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """class city"""
+    state_id = ""
+    name = ""
